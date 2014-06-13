beaebe0703fc6c4117db53ab3ea0095fd151fd37
vagrant <vagrant@ps.steinigke.dev>
2014-06-13 13:53:09 +0000

    Testcode for product conversion

--- application/models/productFromShop.php
+++ application/models/productFromShop.php
@@ -1,19 +1,44 @@
 <?php
 use Bepado\SDK\ProductFromShop;
-use Bepado\SDK\Struct;
+use Bepado\SDK\Struct\Order;
+use Bepado\SDK\Struct\Product;
 
 class oxidProductFromShop implements ProductFromShop
 {
+    protected function _convertToBepadoSdkProduct($id) {
+
+        $sdkProduct = new Product();
+
+        // load oxid article
+        $oxProduct = oxNew('oxarticle');
+        $oxProduct->load($id);
+
+        // @TODO: check if article is marked for bepado
+
+        $sdkProduct->vat = $oxProduct->getArticleVat();
+
+        return $sdkProduct;
+
+    }
+
     public function getProducts(array $ids)
     {
+        $sdkProducts = [];
+
+        foreach ($ids as $id) {
+            $sdkProducts[] = $this->_convertToBepadoSdkProduct($id);
+        }
+
+        return $sdkProducts;
+
     }
     public function getExportedProductIDs()
     {
     }
-    public function reserve(Struct\Order $order)
+    public function reserve(Order $order)
     {
     }
-    public function buy(Struct\Order $order)
+    public function buy(Order $order)
     {
     }
 }
