c90e09b941f5fd62720c086cce0146ca5ad9ef00
Maria Haubner <maria.haubner@mayflower.de>
2014-11-11 11:48:01 +0100

    Added translations in admin-panel

---
+++ application/views/admin/en/module_options.php
@@ -0,0 +1,12 @@
+<?php
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+
+$aLang = array(
+    'charset'                                    => 'ISO-8859-15',
+
+    'SHOP_MODULE_sBepadoLocalEndpoint'           => 'Bepado apiEndpointUrl',
+    'SHOP_MODULE_sBepadoApiKey'                  => 'Bepado apiKey',
+);
