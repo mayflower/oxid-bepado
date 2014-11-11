310dfbbc5fca2c0bf08be91b718c6de2996f24bd
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-11-11 15:06:36 +0100

    improve translations

--- application/views/admin/de/module_options.php
+++ application/views/admin/de/module_options.php
@@ -5,8 +5,10 @@
 // -------------------------------
 
 $aLang = array(
-    'charset'                                    => 'ISO-8859-15',
+    'charset'                          => 'ISO-8859-15',
+    'SHOP_MODULE_sBepadoLocalEndpoint' => 'Bepado apiEndpointUrl',
+    'SHOP_MODULE_sBepadoApiKey'        => 'Bepado apiKey',
+    'SHOP_MODULE_GROUP_main'           => 'Allgemeine Parameter',
+    'api_key_not_verified'             => 'API-Key konnte nicht verifiziert werden und wurde nicht gespeichert.',
 
-    'SHOP_MODULE_sBepadoLocalEndpoint'           => 'Bepado apiEndpointUrl',
-    'SHOP_MODULE_sBepadoApiKey'                  => 'Bepado apiKey',
 );
--- application/views/admin/en/module_options.php
+++ application/views/admin/en/module_options.php
@@ -5,8 +5,9 @@
 // -------------------------------
 
 $aLang = array(
-    'charset'                                    => 'ISO-8859-15',
-
-    'SHOP_MODULE_sBepadoLocalEndpoint'           => 'Bepado apiEndpointUrl',
-    'SHOP_MODULE_sBepadoApiKey'                  => 'Bepado apiKey',
+    'charset'                          => 'ISO-8859-15',
+    'SHOP_MODULE_sBepadoLocalEndpoint' => 'Bepado apiEndpointUrl',
+    'SHOP_MODULE_sBepadoApiKey'        => 'Bepado apiKey',
+    'SHOP_MODULE_GROUP_main'           => 'General Settings',
+    'api_key_not_verified'             => 'API-Key could not be verified and was not saved.',
 );
