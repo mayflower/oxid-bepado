fcb4b32697235b85369b4f6181635ce1aaa1b608
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-11-17 09:09:00 +0100

    redo stuff

--- application/models/productFromShop.php
+++ application/models/productFromShop.php
@@ -1,6 +1,7 @@
 <?php
 
 use Bepado\SDK\ProductFromShop;
+use Bepado\SDK\Struct\Address;
 use Bepado\SDK\Struct\Order;
 
 class oxidProductFromShop implements ProductFromShop
@@ -63,8 +64,66 @@ class oxidProductFromShop implements ProductFromShop
         // werden. Rückgabewert ist die ID der Bestellung
         //
         $oxOrder = oxNew('oxorder'); // ??
+        $products = $order->orderItems;
+        $sdkShop = $order->orderShop;
+
+        $orderValues = array();
+        array_merge($orderValues, $this->convertAddress($order->billingAddress, 'bill'));
+        array_merge($orderValues, $this->convertAddress($order->deliveryAddress, 'del'));
+
+        // set shop id or handle for that
+        // check if the providing shop is the current one
+        // handle provided id????
+
+
+        $oxOrder = oxNew('oxorder');
+        $oxOrder->assign($orderValues);
 
         return $oxOrder->getOxID();
+    }
+
+    /**
+     * Oxid and Bepado got two different address types: delivery and bill. both got an
+     * almost equal structure. So we just transform them in here.
+     *
+     * @param Address $address
+     * @param string  $type
+     *
+     * @return array
+     */
+    private function convertAddress(Address $address, $type)
+    {
+        $oxCountry = oxRegistry::get('oxcountry');
+        $select = $oxCountry->buildSelectString(array('OXISOALPHA3' => $address->country, 'OXACTIVE' => 1));
+        $result =$oxCountry->assignRecord($select);
+        $oxCountryId = $result ? $result->getOxID(): null;
+
+        $oxState = oxRegistry::get('oxstate');
+        $select = $oxState->buildSelectString(array('OXTITLE' => $address->state));
+        $result = $oxState->assignRecord($select);
+        $oxStateId = $oxState ? $result->getOxID() : null;
+
+        $oxAddress = array(
+            'oxorder__ox'.$type.'company'   => $address->company,
+            'oxorder__oxxbilfname'     => $address->firstName.(
+                strlen($address->middleName) > 0
+                    ? ' '.$address->middleName
+                    : ''
+                ),
+            'oxorder__ox'.$type.'name'      => $address->surName,
+            'oxorder__ox'.$type.'street'    => $address->street,
+            'oxorder__ox'.$type.'streetnr'  => $address->streetNumber,
+            'oxorder__ox'.$type.'addinfo'   => $address->additionalAddressLine,
+            'oxorder__ox'.$type.'ustid'     => null,
+            'oxorder__ox'.$type.'city'      => $address->city,
+            'oxorder__ox'.$type.'countryid' => $oxCountryId,
+            'oxorder__ox'.$type.'stateid'   => $oxStateId,
+            'oxorder__ox'.$type.'zip'       => $address->zip,
+            'oxorder__ox'.$type.'fax'       => null,
+            'oxorder__ox'.$type.'sal'       => null,
+        );
+
+        return $oxAddress;
     }
 }
 
