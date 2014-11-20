e2a0dad684dc62159c9ce95cce34af88d4333da2
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-11-20 14:56:28 +0100

    let test work

---
+++ tests/unit/modules/mf_sdk_converterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Maximilian Berghoff <Maximilian.Berghoff@gmx.de>
+ */
+class mf_sdk_converterTest extends PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
+ 
