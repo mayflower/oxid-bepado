8703ccdc63dcbf1d7a2419136fbe05bdb3107ff4
Maria Haubner <maria.haubner@mayflower.de>
2014-11-27 13:38:26 +0100

    * check triggers update an errormessages are stored in array and formatted into html list to display in tpl

--- application/controllers/mf_basket.php
+++ application/controllers/mf_basket.php
@@ -3,9 +3,14 @@
 
 class mf_basket extends mf_basket_parent
 {
+    /**
+     * @var \Bepado\SDK\ProductToShop
+     */
+    private $productToShop;
+
     public function render()
     {
-        parent::render();
+        $parent = parent::render();
 
         $aBasket = $this->_aViewData['oxcmp_basket']->getContents();
 
@@ -15,20 +20,36 @@ class mf_basket extends mf_basket_parent
 
             /** @var mf_bepado_oxarticle $product */
             $product = $basketItem->getArticle();
+            $sdkProduct = $product->getSdkProduct();
+
+            $errorMsg = [];
 
             if ($product->isImportedFromBepado()) {
-                $check = $product->checkProductWithBepardo($product->getSdkProduct());
+                $check = $product->checkProductWithBepardo($sdkProduct);
                 // update sdkProduct
+                foreach ($check as $shopId => $result) {
+                    if ($result === true) {
+                        // everything alright
+                    } else {
+                        $this->productToShop->insertOrUpdate($sdkProduct);
+                        foreach ($result as $message) {
+                            $errorMsg[] = $message;
+                        }
+                    }
+                }
                 $sdkProduct = $product->getSdkProduct();
+                
                 if ($amount > $sdkProduct->availability) {
-                    // request amount bigger than availability
+                   $errorMsg[] = 'This product is available only ' . $sdkProduct->availability . ' times.';
+                }
+
+                if ($errorMsg) {
+                    $checkList = '<ul><li>' . implode('</li><li>', $errorMsg) . '</li></ul>';
+                    $this->_aViewData['checkMsg'] = $checkList;
                 }
             }
         }
 
-        // render error messages into template an show updated products in basket
-        // if order not possible, delete basket item
-
-        return parent::render();
+        return $parent;
     }
 } 
