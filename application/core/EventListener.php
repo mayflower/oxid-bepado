cbfd441b1392912ed25140ffa326d4f2a8d46717
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-12-04 14:12:15 +0100

    create shipping set and rules while creation, map them together with the user group as a starting point

--- application/core/EventListener.php
+++ application/core/EventListener.php
@@ -7,7 +7,7 @@ class EventListener
     /**
      * @var VersionLayerInterface
      */
-    private $_oVersionLayer;
+    public static $_oVersionLayer;
 
     public function onActivate()
     {
@@ -35,6 +35,41 @@ class EventListener
 
             }
         }
+
+        /** @var oxGroups $oxUserGruop */
+        $oxUserGruop = self::getVersionLayer()->createNewObject('oxgroups');
+        $oxUserGruop->load('bepadoshopgroup');
+        if (!$oxUserGruop->isLoaded()) {
+            throw new \Exception('No bepado user group found.');
+        }
+
+        /** @var oxDelivery $oxDelivery */
+        $oxDelivery = self::getVersionLayer()->createNewObject('oxdelivery');
+        $oxDelivery->load('bepadoshippingrule');
+        if (!$oxDelivery->isLoaded()) {
+            throw new \Exception('No bepado shipping found');
+        }
+
+        /** @var oxDeliveryset $oxDeliverySet */
+        $oxDeliverySet = self::getVersionLayer()->createNewObject('oxdeliveryset');
+        $oxDeliverySet->load('bepadoshipping');
+        if (!$oxDeliverySet->isLoaded()) {
+            throw new \Exception('No bepado shipping rule found');
+        }
+
+        $oObject2Delivery = self::getVersionLayer()->createNewObject('oxbase');
+        $oObject2Delivery->init('oxobject2delivery');
+        $oObject2Delivery->oxobject2delivery__oxdeliveryid = new oxField('bepadoshipping');
+        $oObject2Delivery->oxobject2delivery__oxobjectid = new oxField('bepadoshopgroup');
+        $oObject2Delivery->oxobject2delivery__oxtype = new oxField("oxdelsetg");
+        $oObject2Delivery->save();
+
+        $oObject2Delivery = self::getVersionLayer()->createNewObject('oxbase');
+        $oObject2Delivery->init('oxobject2delivery');
+        $oObject2Delivery->oxobject2delivery__oxdeliveryid = new oxField('bepadoshippingrule');
+        $oObject2Delivery->oxobject2delivery__oxobjectid = new oxField('bepadoshipping');
+        $oObject2Delivery->oxobject2delivery__oxtype = new oxField("oxdelset");
+        $oObject2Delivery->save();
     }
 
     /**
--- application/install/bepado.sql
+++ application/install/bepado.sql
@@ -67,11 +67,18 @@ CREATE TABLE IF NOT EXISTS `bepado_product_state` (
   CONSTRAINT pk_p_state PRIMARY KEY (p_source_id, shop_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/* Adds column for the payment type mapping */
 ALTER TABLE oxpayments ADD bepadopaymenttype VARCHAR(100);
 
+/* Special user group for remote shops */
 INSERT INTO oxgroups (`OXID`, `OXACTIVE`, `OXTITLE`) VALUES ('bepadoshopgroup', '0', 'Bepado Remote Shop');
 
+/* Chance to map an user to a bepado shop. All shops will get its own user */
 ALTER TABLE oxuser ADD bepadoshopid VARCHAR(100);
+
+/* Create an bepado shipping type and rule, cause the shop will get access to prices by shipping rules */
+INSERT INTO oxdeliveryset (`OXID`, `OXACTIVE`, `OXTITLE`, `OXSHOPID`) VALUES ('bepadoshipping', '1', 'Bepado Shipping', 'oxbaseshop');
+INSERT INTO oxdelivery (`OXID`, `OXACTIVE`, `OXTITLE`, `OXSHOPID`) VALUES ('bepadoshippingrule', '1', 'Bepado Shipping Rule', 'oxbaseshop');
 
 CREATE TABLE IF NOT EXISTS `bepado_categories` (
   `oxid` CHAR(32) NOT NULL,
