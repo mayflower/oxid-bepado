30d6128d4d6aa9225ba076af5c7cea2db595854a
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-12-15 13:32:14 +0100

    move some lines of code, mfBepado is clean now

--- application/controllers/mfBepado.php
+++ application/controllers/mfBepado.php
@@ -3,43 +3,19 @@
 class mfBepado extends oxUbase
 {
     /**
-     * @var mf_sdk_helper
+     * @var VersionLayerInterface
      */
-    private $_oModuleSdkHelper;
+    private $_oVersionLayer;
 
     public function render() {
         parent::render();
 
-        /** @var mf_sdk_logger_helper $logger */
-        $logger = $this->getVersionLayer()->createNewObject('mf_sdk_logger_helper');
-
-        $sdkConfig = $this->getSdkHelper()->createSdkConfigFromOxid();
-        $sdk = $this->getSdkHelper()->instantiateSdk($sdkConfig);
-        try {
-            echo $sdk->handle(file_get_contents('php://input'), $_SERVER);
-        } catch (\Exception $e) {
-            $logger->writeBepadoLog($e->getMessage());
-        }
+        /** @var mf_sdk_helper $helper */
+        $helper = $this->getVersionLayer()->createNewObject('mf_sdk_helper');
+        $helper->handleRequest();
 
         return $this->_sThisTemplate;
     }
-
-    /**
-     * @return mf_sdk_helper
-     */
-    private function getSdkHelper()
-    {
-        if ($this->_oModuleSdkHelper === null) {
-            $this->_oModuleSdkHelper = oxNew('mf_sdk_helper');
-        }
-
-        return $this->_oModuleSdkHelper;
-    }
-
-    /**
-     * @var VersionLayerInterface
-     */
-    private $_oVersionLayer;
 
     private function getVersionLayer()
     {
