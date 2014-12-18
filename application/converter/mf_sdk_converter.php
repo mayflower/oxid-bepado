8222fd931c7a4d4f29fe930a0db1886bd56585b1
ElectricMaxxx <maximilian.berghoff@gmx.de>
2014-12-18 10:27:18 +0100

    little type in Exception message

--- application/converter/mf_sdk_converter.php
+++ application/converter/mf_sdk_converter.php
@@ -157,7 +157,7 @@ class mf_sdk_converter implements mf_converter_interface
                     list($fieldName, $fieldValue) = $sdkHelper->createOxidImageFromPath($imagePath, $key+1);
                     $aParams[$fieldName] = $fieldValue;
                 } catch (\Exception $e) {
-                    $logger->writeBepadoLog('Image ' . $imagePath . 'could not be saved during product conversion.');
+                    $logger->writeBepadoLog('Image ' . $imagePath . ' could not be saved during product conversion.');
                 }
             }
         }
